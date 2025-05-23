6ab7ce4cac1fc73eb579c6574d0d9fafa6dba969
drb0r1s <drb0r1sdev@gmail.com>
2025-05-23 21:32:21 +0200

    Spawn SolarSystem on touch.

--- A simulator of gravity and visualizing theory of relativity/Assets/Script/ArPlaceObject.cs
+++ A simulator of gravity and visualizing theory of relativity/Assets/Script/ArPlaceObject.cs
@@ -18,7 +18,7 @@ public class ArPlaceObject : MonoBehaviour {
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     void Start() {
-        spawnedLoading = Instantiate(loading);
+        //spawnedLoading = Instantiate(loading);
 
         raycastManager = GetComponent<ARRaycastManager>();
         planeManager = GetComponent<ARPlaneManager>();
@@ -31,24 +31,26 @@ public class ArPlaceObject : MonoBehaviour {
         // We use the center of the screen as the default raycast point.
         Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
 
-        // Here we use TrackableType.PlaneWithinPolygon because we only want to look for plane surfaces to place the solar system on.
-        if(raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon)) {
-            isSolarSystemSpawned = true;
-            Destroy(spawnedLoading);
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            // Here we use TrackableType.PlaneWithinPolygon because we only want to look for plane surfaces to place the solar system on.
+            if(raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon)) {
+                isSolarSystemSpawned = true;
+                //Destroy(spawnedLoading);
         
-            Pose closestPlane = hits[0].pose;
+                Pose closestPlane = hits[0].pose;
 
-            // Moving the solar system slightly up the detected plane.
-            Vector3 placementOffset = new Vector3(0, 0.05f, 0);
-            Vector3 adjustedPosition = closestPlane.position + placementOffset;
+                // Moving the solar system slightly up the detected plane.
+                Vector3 placementOffset = new Vector3(0, 0.05f, 0);
+                Vector3 adjustedPosition = closestPlane.position + placementOffset;
 
-            spawnedSolarSystem = Instantiate(solarSystem, adjustedPosition, closestPlane.rotation);
+                spawnedSolarSystem = Instantiate(solarSystem, adjustedPosition, closestPlane.rotation);
 
-            // Size has to be adjusted for phone view, that way we avoid having too large planets that are too far away.
-            float solarSystemScale = 0.1f;
-            spawnedSolarSystem.transform.localScale = Vector3.one * solarSystemScale;
+                // Size has to be adjusted for phone view, that way we avoid having too large planets that are too far away.
+                float solarSystemScale = 0.1f;
+                spawnedSolarSystem.transform.localScale = Vector3.one * solarSystemScale;
 
-            DisablePlaneDetection();
+                DisablePlaneDetection();
+            }
         }
     }
 
