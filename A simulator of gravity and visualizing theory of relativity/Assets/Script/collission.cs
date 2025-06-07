5a9efed935bd32feb7a4f7a5518c91add6d0bea6
Nikola Gostovikj <nikolagostovic8@gmail.com>
2025-06-07 19:54:21 +0200

    Fixed syntax

--- A simulator of gravity and visualizing theory of relativity/Assets/Script/collission.cs
+++ A simulator of gravity and visualizing theory of relativity/Assets/Script/collission.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using System.Colletions;
-using System.Colletions.Generic;
+using System.Collections;
+using System.Collections.Generic;
 
 public class collision : MonoBehaviour
 {
